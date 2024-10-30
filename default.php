35848470d77e29449a96023112781eda1229684b
unknown <yousef.elshrqawe@gmail.com>
2024-10-30 12:51:19 +0300

    add new site

---
+++ default.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Logo In Browser -->
+    <link rel="sortcut icon" type="image/jpg" href="./media/images/SEVEN BEANS logo-01.png">
+    <!-- Fnt Awesome -->
+    <link rel="stylesheet" href="./CSS/all.min.css">
+    <!-- Render All Element Normally -->
+    <link rel="stylesheet" href="./CSS/normalize.css">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="./CSS/design.css">
+    <!-- Slider File CSS -->
+    <link rel="stylesheet" href="./JS/swiper-bundle.min.css" class="">
+    <!-- SCSS File -->
+    <link rel="stylesheet" href="./CSS/scss/style.css">
+    <title>7 Beans</title>
+</head>
+<body>
+    <!-- Start Landing Page -->
+    <div class="landing">
+        <div class="container">
+            <div class="logo">
+                <img src="./media/logos/SEVEN BEANS logo-01.png" alt="">
+            </div>
+            <div class="left-half"></div>
+            <div class="right-half"></div>
+        </div>
+    </div>
+    <!-- End Landing Page -->
+    <!-- Start Menu -->
+    <div class="menu" >
+        <div class="container">
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">COFFEE FRAPPE</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>FRENCH FRAPPE</span>
+                        <span>CARAMEL FRAPPE</span>
+                        <span>VANILLA FRAPPE</span>
+                        <span>MOCHA FRAPPE</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">BEANS SPECIAL DRINK 7</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>7 MIX ESPRESSO</span>
+                        <span>7 MIX AMERICAN COFFEE</span>
+                        <span>7 MIX TURKISH COFFEE</span>
+                        <span>VELVET LATTE</span>
+                        <span>VELVET FRAPPE</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>2.25</span>
+                            <span>2.25</span>
+                            <span>2.00</span>
+                            <span>3.25</span>
+                            <span>3.50</span>
+                        </div>
+                        <div class="oz2">
+                            <span>--</span>
+                            <span>2.75</span>
+                            <span>--</span>
+                            <span>3.75</span>
+                            <span>3.75</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">HOT DRINKS</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>ESPRESSO DOUBLE</span>
+                        <span>ESPRESSO LUNGO</span>
+                        <span>ESPRESSO MACCHIATO</span>
+                        <span>AMERICAN</span>
+                        <span>RED EYE</span>
+                        <span>LATTE</span>
+                        <span>SPANISH LATTE</span>
+                        <span>COCONUT LATTE</span>
+                        <span>PISTACHIO LATTE</span>
+                        <span>CAPPUCCINO</span>
+                        <span>FLAT WHITE</span>
+                        <span>CORTADO</span>
+                        <span>MACCHIATO</span>
+                        <span>MOCHA</span>
+                        <span>WHITE MOCHA</span>
+                        <span>HOT CHOCOLATE DARK - WHITE</span>
+                        <span>TURKISH COFFEE</span>
+                        <span>TEA</span>
+                        <span>GREEN TEA</span>
+                        <span>GINGER WITH LEMON-HONEY</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>1.75</span>
+                            <span>2.00</span>
+                            <span>2.00</span>
+                            <span>1.75</span>
+                            <span>2.25</span>
+                            <span>2.50</span>
+                            <span>2.75</span>
+                            <span>3.00</span>
+                            <span>3.25</span>
+                            <span>2.50</span>
+                            <span>2.25</span>
+                            <span>2.25</span>
+                            <span>1.75</span>
+                            <span>3.00</span>
+                            <span>3.25</span>
+                            <span>2.75</span>
+                            <span>1.50</span>
+                            <span>1.25</span>
+                            <span>1.25</span>
+                            <span>1.75</span>
+                        </div>
+                        <div class="oz2">
+                            <span>--</span>
+                            <span>--</span>
+                            <span>--</span>
+                            <span>2.25</span>
+                            <span>2.75</span>
+                            <span>2.90</span>
+                            <span>3.25</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.00</span>
+                            <span>2.75</span>
+                            <span>--</span>
+                            <span>--</span>
+                            <span>3.50</span>
+                            <span>3.75</span>
+                            <span>3.25</span>
+                            <span>--</span>
+                            <span>1.50</span>
+                            <span>1.50</span>
+                            <span>2.25</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">COLD DRINKS</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>COLD BREW</span>
+                        <span>MILKSHAKE</span>
+                        <span>ICE COFFEE</span>
+                        <span>ICE MOCHA</span>
+                        <span>ICE CRAMIL MACCHIATO</span>
+                        <span>ICE LATTE</span>
+                        <span>ICE SPANISH LATTE</span>
+                        <span>ICE CARAMEL LATTE</span>
+                        <span>ICE PISTACHIO LATTE</span>
+                        <span>ICE TOFFEE LATTE</span>
+                        <span>ICE MATCHA</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>3.00</span>
+                            <span>3.25</span>
+                            <span>2.00</span>
+                            <span>3.00</span>
+                            <span>3.00</span>
+                            <span>2.75</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>2.50</span>
+                            <span>3.25</span>
+                            <span>3.50</span>
+                            <span>3.25</span>
+                            <span>3.75</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">NON-COFFEE FRAPPE</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>STRAWBERRY CHOCOLATE FRAPPE</span>
+                        <span>CHOCO MINT FRAPPE</span>
+                        <span>BLUE GUM FRAPPE</span>
+                        <span>BOUNTY FRAPPE</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">SMOOTHIES</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>MIX BERRY</span>
+                        <span>MANGO</span>
+                        <span>MINT-LEMON</span>
+                        <span>DRAGON FRUITS</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                            <span>3.25</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                            <span>3.50</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">ENERGY MIX</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>BLUE CURACAO</span>
+                        <span>BUBBLE GUM</span>
+                        <span>MIX BERRY</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>2.75</span>
+                            <span>2.75</span>
+                            <span>2.75</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.00</span>
+                            <span>3.00</span>
+                            <span>3.00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">FRESH JUICE</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>ORANGE</span>
+                        <span>LEMON</span>
+                        <span>LEMON MINT OR BASIL</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>2.75</span>
+                            <span>2.75</span>
+                            <span>2.75</span>
+                        </div>
+                        <div class="oz2">
+                            <span>3.00</span>
+                            <span>3.00</span>
+                            <span>3.00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">SOFT DRINKS</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>WATER 500 ml</span>
+                        <span>ENERGY DRINK</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>0.50</span>
+                            <span>2.50</span>
+                        </div>
+                        <div class="oz2">
+                            <span>--</span>
+                            <span>--</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="beansItems">
+                <div class="bar">
+                    <div class="title">ADDITIONAL</div>
+                    <div class="price">
+                        <span>OZ 12</span>
+                        <span>OZ 12</span>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="items">
+                        <span>ESPRESSO SHOT</span>
+                        <span>FLAVORS</span>
+                        <span>COCONUT MILK</span>
+                        <span>ALMOND MILK</span>
+                    </div>
+                    <div class="cost">
+                        <div class="oz1">
+                            <span>0.50</span>
+                            <span>0.50</span>
+                            <span>0.50</span>
+                            <span>0.50</span>
+                        </div>
+                        <div class="oz2">
+                            <span>0.50</span>
+                            <span>0.50</span>
+                            <span>0.50</span>
+                            <span>0.50</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Menu -->
+    <script src="./JS/main.js"></script>
+</body>
+</html>
